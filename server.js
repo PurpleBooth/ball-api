023417a600dbfbc9e8fb2602dbdda131f12b4f3c
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-07-18 16:49:46 +0100

    Allow CORS

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var _ = require('underscore');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 
 app.use('/', require('./routes/ball'));
